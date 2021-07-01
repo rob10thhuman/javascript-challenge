84a1f367f47d2ac4a4b71afcb6392d47e762c6ab
Rob Thompson <macpro@Robs-Mac-Pro.local>
2021-06-30 21:09:35 -0600

    level 2 push working on logic@

--- UFO-level-2/app.js
+++ UFO-level-2/app.js
@@ -1,6 +1,10 @@
 var tbody=d3.select('tbody');
 var button=d3.select('#filter-btn');
 var inpDT=d3.select('#datetime');
+var inpCity=d3.select('#city')
+var inpST=d3.select('#state')
+var inpCTRY=d3.select('#country')
+var inpSHP=d3.select('#shape')
 var cols=['datetime','city','state','country','shape','durationMinutes','comments']
 
 function tblOn(dataObj){
@@ -17,8 +21,16 @@ button.on('click',() => {d3.event.preventDefault();
 let inpDate=inpDT.property('value')
   
 let fDate=data.filter(data => data.datetime === inpDate);
+let fCity=data.filter(data => data.city === inpCity);
+let fState=data.filter(data => data.state === inpST);
+let fCountry=data.filter(data => data.country === inpCTRY);
+let fShape=data.filter(data => data.shape === inpSHP);
+
+let fData=data.filter(data => data.datetime === inpDate | data.city === inpCity | data.state === inpST | data.country === inpCTRY | data.shape === inpSHP)
+
+// let fData=data.filter(data => fDate || fCity || fState || fCountry || fShape)
 
 tbody.html('');
-  if (fDate.length !==0){tblOn(fDate);}
-  else{tbody.append('tr').append('td').text('Nothing found, earthling.');}
+  if (fData.length!==0){tblOn(fData);}
+  else{tbody.append('tr').append('td').text('Nothing found, Earthling.');}
 })
